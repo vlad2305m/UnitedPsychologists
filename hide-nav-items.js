b59336e6de3e5213b1450fc57cfd800d1c1da84c
vlad2305m <43315575+vlad2305m@users.noreply.github.com>
2022-06-13 22:02:31 +0200

    Update hide-nav-items.js

--- hide-nav-items.js
+++ hide-nav-items.js
@@ -11,7 +11,7 @@ function findAndDelete(el, language) {
   }
 }
 
-fuction hideNavItems(language){ findAndDelete(document.querySelector("#header > div.header-menu.header-menu--folder-list > div.header-menu-nav > nav > div > div.header-menu-nav-folder-content"), language);
+function hideNavItems(language){ findAndDelete(document.querySelector("#header > div.header-menu.header-menu--folder-list > div.header-menu-nav > nav > div > div.header-menu-nav-folder-content"), language);
                              findAndDelete(document.querySelector("#header > div.header-announcement-bar-wrapper > div.header-inner.container--fluid.header-mobile-layout-logo-left-nav-right.header-layout-nav-left > div.header-display-desktop > div.header-title-nav-wrapper > div.header-nav > div > nav"), language);
   }
 export { hideNavItems };
