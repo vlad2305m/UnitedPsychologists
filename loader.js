eca0434427c3d8460ae85f5c3997d8b77459c65e
vlad2305m <43315575+vlad2305m@users.noreply.github.com>
2022-06-13 21:38:33 +0200

    Add hide-nav-items

--- loader.js
+++ loader.js
@@ -1,5 +1,6 @@
 import { setAppointmentButtonText } from 'https://vlad2305m.github.io/UnitedPsychologists/appointment-button.js';
 import { updateLanguageSelectorUrls } from 'https://vlad2305m.github.io/UnitedPsychologists/language-links.js';
+import { hideNavItems } from 'https://vlad2305m.github.io/UnitedPsychologists/hide-nav-items.js';
 
 (function() {
   const languageMatch = window.location.href.match(/psyhelpworld.com\/(..)\/(.*)$/);
@@ -12,6 +13,8 @@ import { updateLanguageSelectorUrls } from 'https://vlad2305m.github.io/UnitedPs
       setAppointmentButtonText(currentLanguage);
       // make language links point to localized current page instead of /home
       updateLanguageSelectorUrls(currentLanguage, currentPage);
+      // hide navigation from other languages
+      hideNavItems(currentLanguage);
     }
   }, false);  
 })();
