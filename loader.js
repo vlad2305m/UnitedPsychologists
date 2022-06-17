9d88ecc7278b2533f8225fe0195646e0943588a4
vlad2305m <43315575+vlad2305m@users.noreply.github.com>
2022-06-17 14:59:18 +0200

    Squash header background image

--- loader.js
+++ loader.js
@@ -19,5 +19,9 @@ hide_nav_items_lock = true;
     hideNavItems(currentLanguage);
     hide_nav_items_lock = false;
     transformLogo();
+    // Squash section background
+      const img = document.querySelector("#sections > section.page-section.layout-engine-section.background-width--full-bleed.section-height--custom.horizontal-alignment--left.vertical-alignment--middle.has-background.black > div > img").parentElement.parentElement;
+      img.style.minHeight = 0;
+      img.children[1].style.display = "none";
   }, false);
 })();
